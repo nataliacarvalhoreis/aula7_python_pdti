f0367ea472a7f4c1e2f449c4ec13afdd551c3040
nataliacarvalhoreis <69061829+nataliacarvalhoreis@users.noreply.github.com>
2020-08-24 16:36:29 -0300

    Add files via upload

--- resposta07.py
+++ resposta07.py
@@ -6,3 +6,57 @@
 # o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde,
 # ou seja, um campo calculado, então não devemos criar um atributo para armazenar
 # esta informação por que ela pode ser calculada a qualquer momento.
+
+class BixinhoVirtual:
+
+    def __init__(self, nome, fome, saude, idade):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
+
+    def alterarNome(self, nome):
+        self.nome = nome
+
+    def alterarFome(self, fome):
+        self.fome = fome
+
+    def alterarSaude(self, saude):
+        self.saude = saude
+
+    def alterarIdade(self, idade):
+        self.idade = idade
+
+    def retornarNome(self):
+        return self.nome
+
+    def retornarFome(self):
+
+        if self.fome == True:
+            return "está com fome"
+        else:
+            return "não está com fome"
+        return self.fome
+
+    def retornarSaude(self):
+        if self.saude == True:
+            return "Boa"
+        else:
+            return "Ruim"
+
+    def retornarIdade(self):
+        return self.idade
+
+    def Humor(self):
+        if(self.saude == True and self.fome == False):
+            humor = "Bom"
+        else:
+            humor = "Ruim"
+        return humor
+
+bixinho = BixinhoVirtual("Tamagushi", False, True, 1)
+
+bixinho.alterarFome(True)
+
+print(f'O bixinho {bixinho.retornarNome()} tem {bixinho.retornarIdade()} ano(s), está com a saúde {bixinho.retornarSaude()} e {bixinho.retornarFome()}')
+print(f'O humor do {bixinho.retornarNome()} está {bixinho.Humor()}')
--- resposta08.py
+++ resposta08.py
@@ -5,45 +5,47 @@
 # diferentes e verificando o conteúdo do estomago a cada refeição.
 # Experimente fazer com que um macaco coma o outro. É possível criar um
 # macaco canibal?
-import random
-import time
 
-class macaco:
-    def __init__(self, nome, bucho=['']):
+class Macaco:
+    def __init__(self, nome, bucho = ""):
         self.nome = nome
-        self.bucho = []
+        self.bucho = bucho
 
-    def comer(self, comida):
-        self.bucho.append(comida)
-        print("Comer.", self.nome)
-        return
+    def comer(self, alimento):
+        if alimento != "":
+            self.bucho = alimento
 
-    def verBucho(self):
-        return self.bucho
 
     def digerir(self):
-        self.bucho = []
+        if(self.bucho != ""):
+            self.bucho = ""
+            return "Digerido!"
+        else:
+            return "Bucho Vazio!"
 
-    def mostra(self):
-        return self.nome
+    def verBucho(self):
+        print(f'Dentro do bucho do macaco {self.nome} tem {self.bucho}')
 
-comidas = ['banana','melão','laranja','mamão','maçã','uva']
-jose = macaco('jose')
-joao = macaco('joao')
+ole = Macaco("Ole")
+pangole = Macaco("Pangole")
 
-while True:
-    print(f"Como estão os macacos")
-    print(f"1 {jose.mostra()}, tem no bucho: {jose.verBucho()}")
-    print(f"2 {joao.mostra()}, tem no bucho: {joao.verBucho()}")
-    time.sleep(1)
+ole.comer("Folha")
+ole.verBucho()
+ole.comer("Banana")
+ole.verBucho()
+ole.comer("Ração")
+ole.verBucho()
+ole.digerir()
+ole.verBucho()
 
-    sorteia = random.randint(1, 5)
+pangole.comer("Folha")
+pangole.verBucho()
+pangole.comer("banana")
+pangole.verBucho()
+pangole.comer("farinha")
+pangole.verBucho()
+pangole.digerir()
+pangole.verBucho()
 
-    if sorteia == 1:  # hora de comer para xita
-        jose.comer(comidas[random.randint(0, len(comidas) - 1)])
-    elif sorteia == 2:  # hora de comer para banze
-        joao.comer(comidas[random.randint(0, len(comidas) - 1)])
-    elif sorteia == 3:  # hora de digestão para xita
-        jose.digerir()
-    elif sorteia == 4:  # hora de digestão para banze
-        joao.digerir()
+ole.comer(pangole)
+ole.verBucho()
